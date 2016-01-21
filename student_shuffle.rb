9816ea0cf86f2adc0e3214c20f08cd6adad59405
Sam Copeland <samcopeland7@gmail.com>
2016-01-21 09:14:15 -0500

    shuffled array

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,4 +1,10 @@
 students = ['Studnut', 'Bobby Sue', 'Spongebob', 'Billy', 'Angelina', 'Brad', 'Georgia', 'Donna', 'Rose']
 
+puts students.objectID
+
+students.shuffle!
+
+puts students.objectID
+
 puts students.to_s
 
