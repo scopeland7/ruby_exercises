2c378f15bcbf438bb34c287e59becf74c8d2a2b3
Sam Copeland <samcopeland7@gmail.com>
2016-01-21 15:46:45 -0500

    Questions using method workable

--- questions_method.rb
+++ questions_method.rb
@@ -1,3 +1,5 @@
+# @clear_screen = "\e[H\e[2J"
+
 @q_and_a = {
 	'What is your name?' => 'sam',
 	'What is your quest?' => 'to seek the grail',
@@ -8,40 +10,56 @@
 
 @score = 0
 
-def ask_question(question)
+def get_answer(question)
 	puts question
-	return question
+	print ":\\"
+	user_answer = gets.chomp.downcase
+	return user_answer
 end
 
 def eval_question(question, answer)
 	status = false
 	if answer == @q_and_a[question]
-		return true
+		status = true
 	end
+	return status
+end
+
+def clear_screen
+	puts "\e[H\e[2J"
 end
 
 def quiz_time!
+	clear_screen
 	puts "I am the bridge keeper. You must answer my questions if you want to cross."
+	gets
+	clear_screen
 	@q_and_a.each do |question, answer|
-		input = ask_question(question)
-		if eval_question(input, answer) == true
+		input = get_answer(question)
+		if eval_question(question, input) == true
 			@score += 1
 		else
+			clear_screen	
 			puts "Off the bridge with you!"
 			break
 		end
+		clear_screen
+	end
+	eval_score(@score, @q_and_a)
+end
+
+def eval_score (s, q_a)
+	if @score == q_a.length
+		puts "Very well! You may pass!"
 	end
 end
 
-def display_score (s, q_a)
-	if @score == q_a.length
-		puts "Very well! You may pass!"
-	else
-
 def main
-	ask_question
+	get_answer
 	eval_question
+	clear_screen
 	quiz_time!
-	puts_score(@score, @question_answers)
+	eval_score(@score, @q_and_a)
+end
 
-
+quiz_time!
